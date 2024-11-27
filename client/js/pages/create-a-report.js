5132f2ff9d70270f60b5243e6bb31fce6609ec52
Tedd Mason <teddmason@gmail.com>
2024-11-27 09:15:07 +0000

    Adding in further components to check your report front end

--- client/js/pages/create-a-report.js
+++ client/js/pages/create-a-report.js
@@ -1,12 +1,40 @@
 const toggleIncidentTypesButton = document.querySelector('#toggle-incident-types')
 const incidentInputs = document.querySelectorAll('input[id^="incidentType"]')
 const divider = document.querySelector('.govuk-radios__divider')
+const checkboxWaterCompany = document.getElementById('water-company')
+const checkboxOtherOrg = document.getElementById('other-org')
+const waterCompanyRadios = document.getElementById('waterCompanyRadios')
+const organisationInput = document.getElementById('organisationInput')
+const timeInput = document.getElementById('timeInput')
+const otherDateTimeInput = document.getElementById('otherDateTimeInput')
+const todayDateInput = document.getElementById('create-date-observed-choice')
+const yesterdayDateInput = document.getElementById('create-date-observed-choice-2')
+const otherDateInput = document.getElementById('create-date-observed-choice-3')
 
+// Events
 toggleIncidentTypesButton.addEventListener('click', (e) => {
   e.preventDefault()
   toggleIncidentTypes()
 })
+checkboxWaterCompany.addEventListener('change', () => {
+  if (checkboxWaterCompany.checked) checkboxOtherOrg.checked = false
+  toggleIncidentTypes()
+})
+checkboxOtherOrg.addEventListener('change', () => {
+  if (checkboxOtherOrg.checked) checkboxWaterCompany.checked = false
+  toggleIncidentTypes()
+})
+todayDateInput.addEventListener('change', () => {
+  toggleDate()
+})
+yesterdayDateInput.addEventListener('change', () => {
+  toggleDate()
+})
+otherDateInput.addEventListener('change', () => {
+  toggleDate()
+})
 
+// funcs
 const toggleIncidentTypes = () => {
   const hideIncidents = toggleIncidentTypesButton.innerText.indexOf('Hide') > -1
   incidentInputs.forEach(input => {
@@ -18,4 +46,34 @@ const toggleIncidentTypes = () => {
   toggleIncidentTypesButton.innerText = toggleIncidentTypesButton.innerText.replace(hideIncidents ? 'Hide' : 'Show', hideIncidents ? 'Show' : 'Hide')
 }
 
-toggleIncidentTypes()
+const toggleWaterAndOrg = () => {
+  if (checkboxWaterCompany.checked) {
+    waterCompanyRadios.style.display = 'block'
+    organisationInput.style.display = 'none'
+  } else if (checkboxOtherOrg.checked) {
+    waterCompanyRadios.style.display = 'none'
+    organisationInput.style.display = 'block'
+  } else if ((!checkboxWaterCompany.checked) && (!checkboxOtherOrg.checked)) {
+    waterCompanyRadios.style.display = 'none'
+    organisationInput.style.display = 'none'
+  }
+}
+
+const toggleDate = () => {
+  if (todayDateInput.checked) {
+    timeInput.style.display = 'block'
+    otherDateTimeInput.style.display = 'none'
+  } else if (yesterdayDateInput.checked) {
+    timeInput.style.display = 'block'
+    otherDateTimeInput.style.display = 'none'
+  } else if (otherDateInput.checked) {
+    timeInput.style.display = 'none'
+    otherDateTimeInput.style.display = 'block'
+  }
+}
+
+window.addEventListener('load', () => {
+  toggleIncidentTypes()
+  toggleWaterAndOrg()
+  toggleDate()
+})
