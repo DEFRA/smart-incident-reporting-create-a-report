12a45f17d5cc42ea4f0925ff1b4e702739a55ffb
sujithvg <sujithvg@users.noreply.github.com>
2024-12-12 17:33:38 +0000

    Fixed sonar issues - Part 6

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -79,93 +79,8 @@ const validateDescription = payload => {
   }
 
   if (payload.descriptionReportedByEmail) {
-    // Validation for date of email
-    const zero = 0
-    const maxMonths = 12
-    const maxDays = 31
-    const maxMinutes = 59
-    const maxHours = 23
-    const firstValidYear = 1900
-    const latestYear = 3000
-    const day = payload.descriptionEmailReportDateDay
-    const month = payload.descriptionEmailReportDateMonth
-    const year = payload.descriptionEmailReportDateYear
-    const validDay = day > zero && day <= maxDays
-    const validMonth = month > zero && month <= maxMonths
-    const validYear = year > firstValidYear && year < latestYear
-    const validDayOnly = validDay && !validMonth && !validYear
-    const validMonthOnly = !validDay && validMonth && !validYear
-    const validYearOnly = !validDay && !validMonth && validYear
-    const validMonthAndYear = !validDay && validMonth && validYear
-    const validDayAndYear = validDay && !validMonth && validYear
-    const validDayAndMonth = validDay && validMonth && !validYear
-    let dateString
-    let validDate = false
-    let isPastDate = false
-    if (validDay && validMonth && validYear) {
-      dateString = `${year}-${month?.padStart(2, '0')}-${day?.padStart(2, '0')}`
-      validDate = moment(dateString, 'YYYY-MM-DD').isValid()
-      const dateToCheck = moment(dateString)
-      const today = moment().startOf('day')
-      isPastDate = dateToCheck.isSame(today, 'day') || dateToCheck.isBefore(today)
-    }
-    const inValidDate = day && month && year && !validDate
-    if (!day && !month && !year) {
-      dateErrorMsg('Enter the date the email was received')
-    } else if (validDate && validDay && validMonth && validYear && !isPastDate) {
-      dateErrorMsg('Date must be in the past')
-    } else if (!day && month && year) {
-      dateErrorMsg('Enter the day the email was received')
-    } else if (day && !month && year) {
-      dateErrorMsg('Enter the month the email was received')
-    } else if (day && month && !year) {
-      dateErrorMsg('Enter the year the email was received')
-    } else if (!day && !month && year) {
-      dateErrorMsg('Enter the day and month the email was received')
-    } else if (day && !month && !year) {
-      dateErrorMsg('Enter the month and year the email was received')
-    } else if (!day && month && !year) {
-      dateErrorMsg('Enter the day and year the email was received')
-    } else if (validMonthAndYear) {
-      dateErrorMsg('Enter a day from 1 to 31')
-    } else if (validDayAndYear) {
-      dateErrorMsg('Enter a month using numbers 1 to 12')
-    } else if (validDayAndMonth) {
-      dateErrorMsg('Enter a full year, for example 2024')
-    } else if (validDayOnly || validMonthOnly || validYearOnly || inValidDate) {
-      dateErrorMsg('The date entered must be a real date')
-    } else {
-      // do nothing (blame sonarcloud)
-    }
-
-    // Validation for time of email
-    if (!payload.descriptionEmailReportTime) {
-      timeErrorMsg('Enter the time the email was received')
-    } else {
-      let validTimeFormat = false
-      const maxTimeLength = 3
-      const time = payload.descriptionEmailReportTime
-      if (moment(time, 'HH:mm').isValid() && time.length >= maxTimeLength) {
-        const timeParts = time.split(':')
-        const hours = timeParts[0]?.padStart(2, '0')
-        const minutes = timeParts[1]?.padStart(2, '0')
-        validTimeFormat = timeParts.length === 2 && (hours >= zero && hours <= maxHours) && (minutes >= zero && minutes <= maxMinutes)
-      }
-
-      if (!validTimeFormat) {
-        timeErrorMsg('Enter a time using the 24-hour clock, from 00:00 for midnight, to 23:59')
-      }
-
-      if (validTimeFormat && validDay && validMonth && validYear && validDate) {
-        const dateTimeString = `${payload.descriptionEmailReportDateYear}-${payload.descriptionEmailReportDateMonth.padStart(2, '0')}-${payload.descriptionEmailReportDateDay.padStart(2, '0')} ${payload.descriptionEmailReportTime}`
-        const dateTime = moment(dateTimeString, 'YYYY-MM-DD hh:mm')
-        const maxAgeMinutes = 5
-        const isDateTimeInPast = dateTime.isBefore(moment().subtract(maxAgeMinutes, 'minutes'))
-        if (!isDateTimeInPast) {
-          timeErrorMsg('Time must be in the past')
-        }
-      }
-    }
+    validateDateOfEmail(payload)
+    validateTimeOfEmail(payload)
   }
 }
 
@@ -191,6 +106,98 @@ const clearErrorList = () => {
   reporter.errorList = []
   location.errorList = []
   date.errorList = []
+}
+
+const validateDateOfEmail = payload => {
+  // Validation for date of email
+  const zero = 0
+  const maxMonths = 12
+  const maxDays = 31
+  const firstValidYear = 1900
+  const latestYear = 3000
+  const day = payload.descriptionEmailReportDateDay
+  const month = payload.descriptionEmailReportDateMonth
+  const year = payload.descriptionEmailReportDateYear
+  const validDay = day > zero && day <= maxDays
+  const validMonth = month > zero && month <= maxMonths
+  const validYear = year > firstValidYear && year < latestYear
+  const validDayOnly = validDay && !validMonth && !validYear
+  const validMonthOnly = !validDay && validMonth && !validYear
+  const validYearOnly = !validDay && !validMonth && validYear
+  const validMonthAndYear = !validDay && validMonth && validYear
+  const validDayAndYear = validDay && !validMonth && validYear
+  const validDayAndMonth = validDay && validMonth && !validYear
+  let dateString
+  let validDate = false
+  let isPastDate = false
+  if (validDay && validMonth && validYear) {
+    dateString = `${year}-${month?.padStart(2, '0')}-${day?.padStart(2, '0')}`
+    validDate = moment(dateString, 'YYYY-MM-DD').isValid()
+    const dateToCheck = moment(dateString)
+    const today = moment().startOf('day')
+    isPastDate = dateToCheck.isSame(today, 'day') || dateToCheck.isBefore(today)
+  }
+  const inValidDate = day && month && year && !validDate
+  if (!day && !month && !year) {
+    dateErrorMsg('Enter the date the email was received')
+  } else if (validDate && validDay && validMonth && validYear && !isPastDate) {
+    dateErrorMsg('Date must be in the past')
+  } else if (!day && month && year) {
+    dateErrorMsg('Enter the day the email was received')
+  } else if (day && !month && year) {
+    dateErrorMsg('Enter the month the email was received')
+  } else if (day && month && !year) {
+    dateErrorMsg('Enter the year the email was received')
+  } else if (!day && !month && year) {
+    dateErrorMsg('Enter the day and month the email was received')
+  } else if (day && !month && !year) {
+    dateErrorMsg('Enter the month and year the email was received')
+  } else if (!day && month && !year) {
+    dateErrorMsg('Enter the day and year the email was received')
+  } else if (validMonthAndYear) {
+    dateErrorMsg('Enter a day from 1 to 31')
+  } else if (validDayAndYear) {
+    dateErrorMsg('Enter a month using numbers 1 to 12')
+  } else if (validDayAndMonth) {
+    dateErrorMsg('Enter a full year, for example 2024')
+  } else if (validDayOnly || validMonthOnly || validYearOnly || inValidDate) {
+    dateErrorMsg('The date entered must be a real date')
+  } else {
+    // do nothing (blame sonarcloud)
+  }
+}
+
+const validateTimeOfEmail = payload => {
+  // Validation for time of email
+  const maxMinutes = 59
+  const maxHours = 23
+  if (!payload.descriptionEmailReportTime) {
+    timeErrorMsg('Enter the time the email was received')
+  } else {
+    let validTimeFormat = false
+    const maxTimeLength = 3
+    const time = payload.descriptionEmailReportTime
+    if (moment(time, 'HH:mm').isValid() && time.length >= maxTimeLength) {
+      const timeParts = time.split(':')
+      const hours = timeParts[0]?.padStart(2, '0')
+      const minutes = timeParts[1]?.padStart(2, '0')
+      validTimeFormat = timeParts.length === 2 && (hours >= zero && hours <= maxHours) && (minutes >= zero && minutes <= maxMinutes)
+    }
+
+    if (!validTimeFormat) {
+      timeErrorMsg('Enter a time using the 24-hour clock, from 00:00 for midnight, to 23:59')
+    }
+
+    if (validTimeFormat && validDay && validMonth && validYear && validDate) {
+      const dateTimeString = `${payload.descriptionEmailReportDateYear}-${payload.descriptionEmailReportDateMonth.padStart(2, '0')}-${payload.descriptionEmailReportDateDay.padStart(2, '0')} ${payload.descriptionEmailReportTime}`
+      const dateTime = moment(dateTimeString, 'YYYY-MM-DD hh:mm')
+      const maxAgeMinutes = 5
+      const isDateTimeInPast = dateTime.isBefore(moment().subtract(maxAgeMinutes, 'minutes'))
+      if (!isDateTimeInPast) {
+        timeErrorMsg('Time must be in the past')
+      }
+    }
+  }
 }
 
 const dateErrorMsg = errMsg => {
