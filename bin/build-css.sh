c08322d671df0262625dbf9d354cd8b71db06e8d
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2025-06-04 21:46:05 +0100

    Use sass not node-sass

--- bin/build-css.sh
+++ bin/build-css.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-node-sass --output-style=expanded \
-  --output=server/public/build/stylesheets \
-  client/sass
+sass --load-path=node_modules/ client/sass:server/public/build/stylesheets
