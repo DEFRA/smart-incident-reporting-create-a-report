1dfbfd2ababa2f65b24c775bd18451cbf305c39d
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2026-01-16 21:03:04 +0000

    Example of querying Graph api for group membership

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,8 +1,19 @@
 import { Boom } from '@hapi/boom'
 import constants from '../utils/constants.js'
+import wreck from '@hapi/wreck'
 
 const handlers = {
   get: async (request, h) => {
+    const response = await wreck.get('https://graph.microsoft.com/v1.0/me/memberOf', {
+      headers: {
+        Authorization: `Bearer ${request.auth.credentials.token}`
+      },
+      json: true
+    })
+
+    const entraGroupIds = response.payload.value.map(group => group.id)
+    console.log('Entra group ids this user is a member of:', entraGroupIds)
+
     if (!request.auth.isAuthenticated) {
       return Boom.unauthorized(`Authentication failed due to: ${request.auth.error.message}`)
     }
