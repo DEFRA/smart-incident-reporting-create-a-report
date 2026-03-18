2695c2f149e45acab85774a13cbba31d0b849500
Scott <scott.dormand@defra.gov.uk>
2026-03-18 07:31:29 +0000

    add privacy notice page from footer

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -17,6 +17,7 @@ const CREATE_A_REPORT = 'create-a-report'
 const CHECK_REPORTER_TYPE = 'check-reporter-type'
 const CHECK_AND_SUBMIT_REPORT = 'check-and-submit-report'
 const REPORT_SUBMITTED = 'report-submitted'
+const PRIVACY_NOTICE = 'privacy-notice'
 
 // Meta data
 const GROUP_MEMBER = 'group-member'
@@ -37,7 +38,8 @@ const views = {
   CHECK_REPORTER_TYPE,
   CHECK_AND_SUBMIT_REPORT,
   CREATE_REPORT_CANCEL,
-  REPORT_SUBMITTED
+  REPORT_SUBMITTED,
+  PRIVACY_NOTICE
 }
 
 const routes = {}
