c3152b374f8cc96e3190ea915aadd36be8c4ab21
sujithvg <sujithvg@users.noreply.github.com>
2026-03-25 17:48:45 +0000

    Updated unit tests and removed map.js file

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ export default {
     core: './client/js/core.js',
     createAReport: './client/js/pages/create-a-report.js',
     checkReporterType: './client/js/pages/check-reporter-type.js',
-    incidentLocationMap: './client/js/map.js'
+    incidentLocationMap: './node_modules/@defra/smart-incident-reporting/client/js/map.js'
   },
   output: {
     path: path.resolve(__dirname, 'server/public/build/js'),
